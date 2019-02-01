2d98b1086504708dda18894661145b0b9f52a49f
imdanielkirk <imdanielkirk@gmail.com>
2019-02-01 00:41:21 -0800

    Added models

---
+++ WebApplication/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class Post
+    {
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ WebApplication/Models/Profile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class Profile
+    {
+        [StringLength(100, MinimumLength = 2)]
+        [Required]
+        public string Name { get; set; }
+
+        public string Teams { get; set; }
+
+        [Required]
+        public string Bio { get; set; }
+
+        [Required]
+        public string Styles { get; set; }
+
+        [Range(0, 90)]
+        [Required]
+        public int Experience { get; set; }
+
+        public DateTime DateJoined { get; set; }
+
+    }
+}
